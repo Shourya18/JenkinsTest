662fceb478e441c5af8ea8e906b82569427365f9
Shourya <shouryavir2012@gmail.com>
2020-12-21 14:11:57 +0530

    the basic files for test

---
+++ src/main/java/testProjectJenkins/JenkinsTest/App.java
@@ -0,0 +1,13 @@
+package testProjectJenkins.JenkinsTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/testProjectJenkins/JenkinsTest/AppTest.java
@@ -0,0 +1,20 @@
+package testProjectJenkins.JenkinsTest;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
